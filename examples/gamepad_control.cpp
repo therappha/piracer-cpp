ab790903374a637cf617ceb2e8e2732dc0d2bf29
therappha <therappha@gmail.com>
2025-10-23 16:27:48 +0100

    debug functions

--- examples/gamepad_control.cpp
+++ examples/gamepad_control.cpp
@@ -1,9 +1,9 @@
 #include "../PiRacer/PiRacer.hpp"
 #include "../Gamepad/ShanwanGamepad.hpp"
 
+
 int main()
 {
-	// Ensure GPIO is initialized
 	
 
 	// Create instances
@@ -14,16 +14,24 @@ int main()
 	{
 		ShanWanGamepadInput input = gamepad.read_data();
 
-		float steering = input.analog_stick_left.x;
-		float throttle = input.analog_stick_right.y * 0.5; // limit throtle to 50%
+		float steering = -input.analog_stick_left.x;
+		float throttle = input.analog_stick_right.y; // limit throttle to 100%
 
 		std::cout << "Throttle: " << throttle
 				<< ", Steering: " << steering
 				<< std::endl;
 		
-		racer.setSteeringPercent(steering);
-		racer.setThrottlePercent(throttle);
+		// racer.setSteeringPercent(steering);
+		// racer.setThrottlePercent(throttle * 0.5);
+		if (input.button_x) 
+		{
+			std::cout << "Exiting..." << std::endl;		
+			break;
+		}
+		sleep(0.1);
 	}
+	// racer.setSteeringPercent(0.0);
+	// racer.setThrottlePercent(0.0);
 
 	return 0;
 }
